91cb7d97ae868170b3024fac873869eff4d38384
Prajapati Akash <prajapati.akash16043@gmail.com>
2023-04-01 17:43:13 +0530

    registration form change

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,58 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <div class="row">
+      <div class="col align-self-center col-3">
         <form @submit.prevent="onSubmit">
-          <div class="col-3">
-            <p>user name</p>
-            <input type="text"  name="user_name" v-model="user_name"/>
-            <p>email</p>
-            <input type="text"  name="email" v-model="email"/>
-            <p>password</p>
-            <input type="password"  name="password" v-model="password"/>
+            <div class="row">
+                <div class="col-3 col-md-6 col-sm-9">
+                  <p>user name</p>
+                  <input type="text"  name="user_name" v-model="user_name"/>
+                  <!-- <div v-if="v$.name.$error">Name field has an error.</div> -->
+                </div>
+            </div>
 
-            <button class="btn btn-primary">Submit</button>
-            <button type="reset" class="btn btn-secondary">Reset</button>
-          </div>
-        </form>
+            <div class="row">
+                <div class="col-3 col-md-6 col-sm-9">
+                  <p>email</p>
+                  <input type="text"  name="email" v-model="email"/>
+                  <!-- <div v-if="v$.name.$error">Name field has an error.</div> -->
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-3 col-md-6 col-sm-9">
+                  <p>password</p>
+                  <input type="password"  name="password" v-model="password"/>
+                  <!-- <div v-if="v$.name.$error">Name field has an error.</div> -->
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3 col-md-6 col-sm-9 pt-3">
+                  <div class="d-flex flex-row bd-highlight mb-3">
+                    <div class="p-2 bd-highlight">
+                      <button class="btn btn-primary">Submit</button>
+                    </div>
+                    <div class="p-2 bd-highlight">
+                      <button type="reset" class="btn btn-secondary">Reset</button>
+                    </div>
+                  </div> 
+                </div>
+            </div>
+
+            
+           
+
+           
+          </form>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 export default {
   name: 'Register',
@@ -26,9 +60,13 @@ export default {
     
   },
   components : {
+    useVuelidate,
+    required,
+    email
   },
   data() {
     return {
+      v$: useVuelidate(),
       user_name : "",
       email : "",
       password : ""
