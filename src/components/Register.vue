35c8275b64b4b7a0e998c556d8f9719ad6885bd5
Prajapati Akash <prajapati.akash16043@gmail.com>
2023-04-01 19:50:43 +0530

    registration form change

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,16 +6,20 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
                   <label for="first_name" class="form-label" placeholder="name">user name</label>
-                  <input type="text"  name="first_name" v-model="first_name" class="form-control"/>
-                  <span v-if="v$.first_name.$error"> {{ v$.first_name.$errors[0].$message }}</span>
+                  <input type="text"  name="first_name" v-model="v$.first_name.$model" class="form-control"/>
+                  <div class="tex-danger" v-for="(error, index) of v$.first_name.$errors" :key="index">
+                    <div class="error-msg">{{ error.$message }}</div>
+                  </div>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
                   <label for="email" class="form-label">email</label>
-                  <input type="text"  name="email" v-model="email" class="form-control"/>
-                  <span v-if="v$.email.$error"> {{ v$.email.$errors[0].$message }}</span>
+                  <input type="text"  name="email" v-model="v$.email.$model" class="form-control"/>
+                  <div class="tex-danger" v-for="(error, index) of v$.email.$errors" :key="index">
+                    <div class="error-msg">{{ error.$message }}</div>
+                  </div>
                 </div>
             </div>
             
@@ -45,32 +49,43 @@
 </template>
 
 <script>
-import useValidate from '@vuelidate/core'
-import { required } from '@vuelidate/validators';
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 export default {
+  name: 'Register',
+  props: {
+    
+  },
+  components : {
+    useVuelidate,
+    required,
+    email
+  },
+  setup () {
+    return { v$: useVuelidate() }
+  },
   data() {
     return {
-      v$: useValidate(),
-      first_name:'',
-      email:'',
-      password:'',
-    }
-  },
-  validation(){
-    return {
-      first_name:{required},
-      email:{required},
-      password:{required},
+      first_name : "",
+      email : "",
+      password : ""
     }
   },
   methods: {
     onSubmit() {
-      this.v$.$validate();
-      if(!this.v$.$error) {
-        console.log('submit form');
-      } else {
-        console.log('form not submitted');
+      console.log('console a submit button');
+      
+      console.log(typeof this.v$.$validate());
+    }
+  },
+  validations () {
+    return {
+      first_name: { 
+        required
+      },
+      email: { 
+        required
       }
     }
   }
