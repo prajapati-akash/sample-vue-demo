070f19d5c4b0f8fb7c8f63f910ecbe7c0d0d72e5
Prajapati Akash <prajapati.akash16043@gmail.com>
2023-04-01 17:06:03 +0530

    registration form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Register msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from './components/Register.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Register
   }
 }
 </script>
---
+++ src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <div class="row">
+        <form @submit.prevent="onSubmit">
+          <div class="col-3">
+            <p>user name</p>
+            <input type="text"  name="user_name" v-model="user_name"/>
+            <p>email</p>
+            <input type="text"  name="email" v-model="email"/>
+            <p>password</p>
+            <input type="password"  name="password" v-model="password"/>
+
+            <button class="btn btn-primary">Submit</button>
+            <button type="reset" class="btn btn-secondary">Reset</button>
+          </div>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Register',
+  props: {
+    
+  },
+  components : {
+  },
+  data() {
+    return {
+      user_name : "",
+      email : "",
+      password : ""
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('console a submit button');
+      console.log(values.type);
+    }
+  }
+}
+</script>
